04ffa8fe9d0b094925e65cf6c685b7b0bcc76f08
Htiango <hongty106@gmail.com>
2017-12-03 11:32:55 -0500

    able to evaluate by reading binary boundary file

--- main/evaluation.py
+++ main/evaluation.py
@@ -104,5 +104,69 @@ def main():
     f.close()
 
 
+def evaluate_from_image():
+    OUTPUT_PATH_KMEANS = '../output/kmeans-seg/'
+    OUTPUT_PATH_SHIFT = '../output/meanshift-seg/'
+    OUTPUT_PATH_TRUTH = '../output/truth-seg/'
+
+    PATH = OUTPUT_PATH_KMEANS
+
+    OUTPUT_LOG = '../output/log.txt'
+
+    f = open(OUTPUT_LOG, "w+")
+
+    start = time.time()
+    # get all the boundary file
+    boundary_dir = "../data/groundTruth/train/"
+    boundary_files = [f for f in listdir(boundary_dir) if isfile(join(boundary_dir, f)) and f.endswith(".mat")]
+
+    f.write("Get a list of images and boundary file\n")
+
+    num = len(boundary_files)
+
+    f.write("Image num is: " + str(num) + "\n\n")
+
+    total_precision = 0.0
+    total_recall = 0.0
+
+    for i in range(num):
+        start_iteration = time.time()
+        print("Handling:   " + boundary_files[i] )
+        f.write("Handling:   " + boundary_files[i] + "\n")
+
+        name, extension = splitext(boundary_files[i])
+
+        # img_path = join(PATH, name+'-boundary-shift.png')
+
+        img_path = join(PATH, name + '-boundary-kmeans.png')
+
+        boundary_path = join(boundary_dir, boundary_files[i])
+
+        img = cv2.imread(img_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        size = img.shape
+        boundary_predict_shift = img.reshape(size[0], size[1], 1)
+
+        boundary_truth = get_groundTruth(boundary_path)
+
+        precision, recall = eval_bound(boundary_predict_shift, boundary_truth, 4)
+        print("Precision is: " + str(precision))
+        print("Recall is:" + str(recall))
+
+        f.write("Precision is: " + str(precision) + "\n")
+        f.write("Recall is:" + str(recall) + "\n\n")
+
+        total_precision += precision
+        total_recall += recall
+
+    average_precision = total_precision / num
+    average_recall = total_recall / num
+    print("Average precision is: " + str(average_precision))
+    print("Average recall is:" + str(average_recall))
+    f.write("Average precision is: " + str(average_precision) + "\n")
+    f.write("Average recall is:" + str(average_recall) + "\n")
+    f.close()
+
+
 if __name__ == "__main__":
-    main()
+    evaluate_from_image()
